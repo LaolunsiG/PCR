c69a2597e3870bf872028612bd47123eef336a73
XiaoE <151894588+LaolunsiG@users.noreply.github.com>
2025-01-26 20:34:14 +0800

    Update and rename CLash-Verge.js to CLash.Meta_Override.js

--- Scripts/Clash/CLash-Verge.js
+++
@@ -1,8 +0,0 @@
-// name: CLash-Verge
-// author: XiaoE
-// updata: 2025-01-26
-// update_log: 
-// updata_url: https://raw.githubusercontent.com/LaolunsiG/PCR/refs/heads/main/Scripts/Clash/CLash-Verge.js
-// repo: https://github.com/LaolunsiG/PCR/tree/main/Scripts/Clash
-
-
