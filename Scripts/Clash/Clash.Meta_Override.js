c518737ec358848b5b552610cb40b3680e400a16
XiaoE <151894588+LaolunsiG@users.noreply.github.com>
2025-01-28 20:02:54 +0800

    Update and rename CLash.Meta_Override.js to Clash.Meta_Override.js

--- Scripts/Clash/Clash.Meta_Override.js
+++ Scripts/Clash/Clash.Meta_Override.js
@@ -2,7 +2,7 @@
 // author: XiaoE
 // updata: 2025-01-26
 // update_log: 
-// updata_url: https://raw.githubusercontent.com/LaolunsiG/PCR/refs/heads/main/Scripts/Clash/CLash.Meta_Override.js
+// updata_url: https://raw.githubusercontent.com/LaolunsiG/PCR/refs/heads/main/Scripts/Clash/Clash.Meta_Override.js
 // repo: https://github.com/LaolunsiG/PCR/tree/main/Scripts/Clash
 
 
